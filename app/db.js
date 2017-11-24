18c8308265638f28bdc291b6dfda5d1fbf3891ff
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2017-11-24 17:11:54 +0100

    drafr social-media-app server

---
+++ app/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/social', function(){
+    console.log.bind(console,"Connected!");    
+});
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
---
+++ app/server.js
@@ -0,0 +1,72 @@
+"use strict"
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const log = console.log;
+const Post = require('./modules/post');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin' , '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get('/api/posts', function (req, res, next) {
+    Post.find(function (err, posts) {
+        if (err) {
+            return next(err);
+        }        
+        res.status(200).json(posts);
+    });
+});
+
+app.post('/api/posts', function (req, res, next) {
+    const post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    });
+
+    post.save(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+        res.status(201).json(post);
+    });
+});
+
+app.delete('/api/posts', function (req, res, next) {
+    Post.findByIdAndRemove(req.body._id,function(err){
+        if(err){
+            return next(err);
+        }
+        res.status(204).end();
+    });
+});
+
+app.put('/api/posts', function (req, res, next) {
+    console.log(req.body);
+    Post.findByIdAndUpdate(
+        req.body._id,
+        {
+            username: req.body.username,
+            body: req.body.body
+        },
+        function(err){
+            if(err){
+                return next(err);
+            }
+            res.status(204).end();
+        }
+    );
+});
+
+app.listen(3000, function () {
+    log('Server is listening at port number', 3000);
+});
+
+module.exports = app;
